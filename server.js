aa6437c7f0bed676812c8006baa84b1b252add02
kmvanneste <kmvanneste17@gmail.com>
2020-08-22 18:22:57 -0400

    got post and delete routes to work

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require("express");
-var path = require("path");
-//var noteData = require("./db/db.json");
+const path = require("path");
+const noteData = require("./db/db.json");
 const fs = require("fs");
+const uuid = require("uuid/v1");
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Read db.json using fs.readFileSync and JSON.parse()
-let dataStr = fs.readFileSync("db/db.json", "utf8");
-console.log("dataStr", dataStr);
-let noteArray = JSON.parse(dataStr);
-console.log("noteArray", noteArray);
-
-// noteArray.forEach(note) {
-//   //give unique
-// };
+//let dataStr = fs.readFileSync("db/db.json", "utf8");
+//let noteArray = JSON.parse(dataStr);
 
 //HTML ROUTES --------------------------------------------------------
 app.get("/", function (req, res) {
@@ -34,25 +29,38 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes/", function (req, res) {
   console.log("getting note");
   res.json(noteArray);
+});
+
+app.post("/api/notes", function (req, res) {
+  console.log(req.body);
+  let note = req.body;
+  const { title, text } = note;
+  let newNote = { title, text, id:uuid() };
+
+  console.log(newNote);
+  noteArray.push(newNote);
+  res.json(noteArray);
+  console.log("saving note");
 
   fs.writeFile("./db/db.json", JSON.stringify(noteArray), function (err) {
     if (err) throw err;
     console.log("sucessful write");
- });
- 
-});
-
-app.post("/api/notes", function (req, res) {
-  
-  let newNote = req.body;
-  noteArray.push(newNote);
-  res.json(noteArray);
-  console.log("saving note");
+  });
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-  console.log("delete post");
-  
+  let id = req.params.id;
+  let notes = noteArray;
+  console.log(notes);
+  let filteredNotes = noteArray.filter((note) => note.id !== id);
+  console.log(filteredNotes);
+  fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), function (err) {
+    if (err) throw err;
+    console.log("sucessful delete");
+  });
+  window.location.reload();
+  //console.log(noteArray);
+  console.log("delete post"); 
 })
 
 // If no matching route is found default to home
